062dff6dc3a848e3d143a38b663bf3491c5a26bc
aLe3ouLa <ale3mp90@gmail.com>
2020-04-28 18:59:49 +0200

    Fix zero values

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,9 +1,11 @@
 export const convert = (number) => {
+  if (Number(number) === 0 || number === "") return "";
   const decimal = (number.toString().split(".")[1] || []).length;
   return decimal > 2 ? Number(number).toFixed(2) : number;
 };
 
 export const convertWithZero = (number) => {
+  if (Number(number) === 0 || number === "") return "";
   const decimal = (number.toString().split(".")[1] || []).length;
   return decimal >= 0 ? Number(number).toFixed(2) : number;
 };
